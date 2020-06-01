4cc9f16b1472360a83684ba65603a4bba452ea16
Hitesh <hitesh@nutrabay.com>
2020-06-01 20:50:47 +0530

    create products model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# Products Model
+class Products(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField(max_length=10)
+    date_created = models.DateField()
